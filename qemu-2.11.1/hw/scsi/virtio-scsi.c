14dcadc41321095ae4028709ae184aaf9fd03d63
sgFlags <wb2331@columbia.edu>
2018-04-02 20:22:43 -0400

    debug qemu

--- qemu-2.11.1/hw/scsi/virtio-scsi.c
+++ qemu-2.11.1/hw/scsi/virtio-scsi.c
@@ -626,6 +626,8 @@ static void virtio_scsi_handle_cmd(VirtIODevice *vdev, VirtQueue *vq)
     /* use non-QOM casts in the data path */
     VirtIOSCSI *s = (VirtIOSCSI *)vdev;
 
+    printf("in virtio_scsi_handle_cmd\n");
+
     if (s->ctx) {
         virtio_device_start_ioeventfd(vdev);
         if (!s->dataplane_fenced) {
