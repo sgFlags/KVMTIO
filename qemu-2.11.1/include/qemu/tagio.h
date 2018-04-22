f6e30099d00a0676542dd2661c27eb9bbc215d00
sgFlags <wb2331@columbia.edu>
2018-04-22 14:44:08 -0400

    debug

---
+++ qemu-2.11.1/include/qemu/tagio.h
@@ -0,0 +1,19 @@
+#ifndef _TAGIO_H
+#define _TAGIO_H
+
+#include <stdint.h>
+#include <sys/types.h>
+
+#define FLAG_TAG    0x10000
+#define FLAG_OK     0x1000
+
+
+struct tag_data {
+    uint8_t prio;
+    pid_t vm_pid;
+    pid_t proc_pid;
+    uint32_t tag_flags;
+};
+
+
+#endif
